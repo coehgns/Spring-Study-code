a06f580d0b1aac077086e196979d2acb1fe164c5
coehgns <modooboiroo@gmail.com>
2024-08-10 22:05:41 +0900

    feat: Board 엔티티 추가

---
+++ src/main/java/com/example/springstudycode/entity/Board.java
@@ -0,0 +1,29 @@
+package com.example.springstudycode.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 20)
+    private String title;
+
+    @Column(name = "content", nullable = false, length = 200)
+    private String content;
+
+    public void updateBoard(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+    
+}
