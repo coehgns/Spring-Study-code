608fb816a2aa16b3e627a32f7c34bf5cf4f17520
coehgns <modooboiroo@gmail.com>
2024-08-11 21:28:03 +0900

    feat: BoardCreateRequest 재추가

--- src/main/java/com/example/springstudycode/domain/board/controller/dto/request/BoardCreateRequest.java
+++ src/main/java/com/example/springstudycode/domain/board/controller/dto/request/BoardCreateRequest.java
@@ -1,2 +1,15 @@
-package com.example.springstudycode.domain.board.controller.dto.request;public class BoardCreateRequest {
+package com.example.springstudycode.domain.board.controller.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardCreateRequest {
+
+    private String title;
+
+    private String content;
+
 }
