9df2036cb85d0c7e5386d03e439078b82dfe9ea5
coehgns <modooboiroo@gmail.com>
2024-08-12 08:22:44 +0900

    feat: BoardService 추가

--- src/main/java/com/example/springstudycode/domain/board/service/BoardService.java
+++ src/main/java/com/example/springstudycode/domain/board/service/BoardService.java
@@ -1,11 +1,57 @@
 package com.example.springstudycode.domain.board.service;
 
+import com.example.springstudycode.domain.board.controller.dto.request.BoardCreateRequest;
+import com.example.springstudycode.domain.board.controller.dto.request.BoardUpdateRequest;
+import com.example.springstudycode.domain.board.controller.dto.response.BoardResponse;
+import com.example.springstudycode.domain.board.entity.Board;
 import com.example.springstudycode.domain.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class BoardService {
     private BoardRepository boardRepository;
 
-    public void addBoard()
+    public void addBoard(BoardCreateRequest request) {
+
+        boardRepository.save(
+                Board.builder()
+                        .title(request.getTitle())
+                        .content(request.getContent())
+                        .build()
+        );
+    }
+
+    public BoardResponse onlyFindBoard(Long bordId) {
+        Board board = boardRepository.findById(bordId)
+                .orElseThrow(() -> new RuntimeException("not found"));
+
+        return BoardResponse.builder()
+                .title(board.getTitle())
+                .content(board.getContent())
+                .build();
+    }
+
+    @Transactional
+    public void modifyBoard(Long boardId, BoardUpdateRequest request) {
+
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new RuntimeException("not found."));
+
+        board.updateBoard(request.getTitle(), request.getContent());
+
+    }
+
+    public void deleteBoard(Long boardId) {
+
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new RuntimeException("not found."));
+
+        boardRepository.delete(board);
+    }
+
 }
