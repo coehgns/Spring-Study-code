becf3e0f615a64877c2f32094f60c485c2a1b06f
coehgns <modooboiroo@gmail.com>
2024-08-12 22:17:28 +0900

    chore: ErrorResponse 수정

--- src/main/java/com/example/springstudycode/global/error/ErrorResponse.java
+++ src/main/java/com/example/springstudycode/global/error/ErrorResponse.java
@@ -1,25 +1,20 @@
 package com.example.springstudycode.global.error;
 
-import com.example.springstudycode.global.error.exception.ErrorCode;
 import lombok.*;
 
-import java.time.LocalDateTime;
 
 @Getter
 @Builder
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class ErrorResponse {
 
-    private LocalDateTime timestamp = LocalDateTime.now();
     private int status;
     private String message;
 
-    public static ErrorResponse of(ErrorCode errorCode) {
+    public static ErrorResponse of(String message, int status) {
         return  ErrorResponse.builder()
-                .message(errorCode.getMessage())
-                .status(errorCode.getStatusCode())
-                .timestamp(LocalDateTime.now())
+                .message(message)
+                .status(status)
                 .build();
     }
 }
