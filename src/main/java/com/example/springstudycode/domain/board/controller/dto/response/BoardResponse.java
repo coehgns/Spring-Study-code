ebfd5e910876598e6a00e389f25fb7f08d747247
coehgns <modooboiroo@gmail.com>
2024-08-12 12:42:22 +0900

    feat: BoardResponse 추가

--- src/main/java/com/example/springstudycode/domain/board/controller/dto/response/BoardResponse.java
+++ src/main/java/com/example/springstudycode/domain/board/controller/dto/response/BoardResponse.java
@@ -1,10 +1,18 @@
 package com.example.springstudycode.domain.board.controller.dto.response;
 
 
+import com.example.springstudycode.domain.board.entity.Board;
 import lombok.Builder;
 
 @Builder
 public class BoardResponse {
     private String title;
     private String content;
+
+    public static BoardResponse boardResponse(Board board) {
+        return BoardResponse.builder()
+                .content(board.getContent())
+                .title(board.getTitle())
+                .build();
+    }
 }
